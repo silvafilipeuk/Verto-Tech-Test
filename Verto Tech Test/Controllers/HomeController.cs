8d2c177f04b5d7cb7ab649834702f789703accd1
Filipe <silvafilipeuk@gmail.com>
2024-07-10 18:33:45 +0100

    Add project files.

---
+++ Verto Tech Test/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Verto_Tech_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
