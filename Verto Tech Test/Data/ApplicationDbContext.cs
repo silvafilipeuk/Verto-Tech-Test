1b668704e05410fc72940bdb184754339fd6cd51
Filipe <silvafilipeuk@gmail.com>
2024-07-11 20:35:48 +0100

    Created a Migration to seed the database with initial data.

--- Verto Tech Test/Data/ApplicationDbContext.cs
+++ Verto Tech Test/Data/ApplicationDbContext.cs
@@ -10,5 +10,39 @@ namespace Verto_Tech_Test.Data
         }
 
         public DbSet<Page> Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Page>().HasData(
+                new Page
+                {
+                    Id = new Guid(Guid.NewGuid().ToString()),
+                    Title = "New Products",
+                    ImageUrl = "https://placehold.co/300x230",
+                    Description = "Lorem ipsum dolor sit amet, id per dictas inermis. Eam odio modo cibo at. Purto dignissim euripidis eu mel, cu mel."
+                },
+                new Page
+                {
+                    Id = new Guid(Guid.NewGuid().ToString()),
+                    Title = "Field Events",
+                    ImageUrl = "https://placehold.co/300x230",
+                    Description = "Lorem ipsum dolor sit amet, id per dictas inermis. Eam odio modo cibo at. Purto dignissim euripidis eu mel, cu mel."
+                },
+                new Page
+                {
+                    Id = new Guid(Guid.NewGuid().ToString()),
+                    Title = "Latest News",
+                    ImageUrl = "https://placehold.co/300x230",
+                    Description = "Lorem ipsum dolor sit amet, id per dictas inermis. Eam odio modo cibo at. Purto dignissim euripidis eu mel, cu mel."
+                },
+                new Page
+                {
+                    Id = new Guid(Guid.NewGuid().ToString()),
+                    Title = "Gallery",
+                    ImageUrl = "https://placehold.co/300x230",
+                    Description = "Lorem ipsum dolor sit amet, id per dictas inermis. Eam odio modo cibo at. Purto dignissim euripidis eu mel, cu mel."
+                }
+                );
+        }
     }
 }
