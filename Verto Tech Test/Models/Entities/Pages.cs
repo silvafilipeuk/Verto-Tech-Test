4c8df5eca5eafe20c790120387a1c71559370e79
Filipe <silvafilipeuk@gmail.com>
2024-07-11 08:19:23 +0100

    Implemented product Categories Section.

---
+++ Verto Tech Test/Models/Entities/Pages.cs
@@ -0,0 +1,11 @@
+﻿namespace Verto_Tech_Test.Models.Entities
+{
+    public class Pages
+    {
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
